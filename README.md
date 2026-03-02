861c55525d8477310a0923c365c65026b0e47924
SenukaR <137078034+SenukaR@users.noreply.github.com>
2026-03-02 13:29:04 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 University project for the collaboration module.
 
 ## Website Links
-WLV MiLinux: https://mi-linux.wlv.ac.uk/~2432452/Collab/index.php \
+WLV MiLinux: https://mi-linux.wlv.ac.uk/~2432452/WolfCentral/index.php \
 XAMPP LocalHost: http://localhost/WolfCentral/index.php
--- db.php
+++ db.php
@@ -1,27 +1,36 @@
 <?php
-// Set this to true when at home to run the site with XAMPP
-// Set this to false when at University to run the site with MI-Linux
-$is_home = true;
+// Disable default error reporting for mysqli to handle it manually
+mysqli_report(MYSQLI_REPORT_OFF);
 
-if ($is_home) {
-    // HOME SETTINGS (XAMPP)
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "db2432452";
-} else {
-    // UNIVERSITY SETTINGS (Linux Server)
+try {
+    // 1. TRY UNIVERSITY SETTINGS (Linux Server)
     $host = "localhost";
     $user = "2432452";
     $pass = "zhnukv";
-    $db = "db2432452";
+    $db   = "db2432452";
+
+    $mysqli = new mysqli($host, $user, $pass, $db);
+
+    // If there is a connection error, throw an exception to trigger the 'catch' block
+    if ($mysqli->connect_errno) {
+        throw new Exception($mysqli->connect_error);
+    }
+
+} catch (Exception $e) {
+    // 2. FALLBACK TO HOME SETTINGS (XAMPP)
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "db2432452";
+
+    $mysqli = new mysqli($host, $user, $pass, $db);
+
+    // Final check: If both fail, then we die
+    if ($mysqli->connect_errno) {
+        die("Database Connection Failed on both environments: " . $mysqli->connect_error);
+    }
 }
 
-// Create connection
-$mysqli = new mysqli($host, $user, $pass, $db);
-
-// Check connection
-if ($mysqli->connect_errno) {
-    die("Database Connection Failed: " . $mysqli->connect_error);
-}
+// Re-enable standard error reporting for your queries
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 ?>
